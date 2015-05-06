6704c9519f623d47a50c67cbfc9adef8d3568cff
mlg77 <mlg77@uclive.ac.nz>
2015-05-06 14:51:06 +1200

    PDEpe Function
    Attempts to use pdepe function doesn't work

---
+++ PDE/icfun1.m
@@ -0,0 +1,14 @@
+function [ u ] = icfun1( x )
+%icfun1 the partial differential equations Inital Conditions
+%   Attempt 1 
+%   Michelle Goodman
+%   21/4/2015
+
+%u = 0*x;
+u1 = 400e-6;
+u2 = 400e-6;
+
+u = [u1; u2];
+
+end
+
