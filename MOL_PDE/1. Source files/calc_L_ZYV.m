598b485c9a401042b29bc7321801fe31ef653971
mlg77 <mlg77@uclive.ac.nz>
2016-05-02 01:39:44 +1200

    Clean Compatible
    Moved files to Old_Method leaving only compatible files visable

--- MOL_PDE/1. Source files/calc_L_ZYV.m
+++ MOL_PDE/1. Source files/calc_L_ZYV.m
@@ -1,4 +1,4 @@
-function [L_Z, L_V, L_Y] = calc_L_ZYV(Z, V, Y, beta)
+function [L_Z, L_V, L_Y, v_2, v_3] = calc_L_ZYV(Z, V, Y, beta)
 %Goldbeter Equations Set
 %   Calculates the non linear concentration rates for the calciun in the
 %   cytocol, Z, the calcium in the stores, and the membrain potential.
@@ -27,10 +27,23 @@ v_1 = 7.3;
 v_2 = V_m2*Z.^n./(K_2^n + Z.^n);
 v_3 = V_m3.*(Y.^n./(K_r^n + Y.^n)).*(Z.^p./(K_a^p + Z.^p));
 
-%% From Goldbeter
-L_Z =  v_0 + v_1*beta - v_2 + v_3 + k_f*Y - k*Z;
-L_V =  F* V_cyto*(v_0 - k*Z + v_1*beta);
+%% VOCC
+G_Ca = 1.29e-3;
+V_Ca_1 = 100;
+V_Ca_2 = -24;
+R_Ca = 8.5;
+VOCC =G_Ca *(V - V_Ca_1)./(1+ exp(-(V-V_Ca_2)/R_Ca));
+
+% VOCC = 0;
+%% New L equations
+L_Z =  v_0 + v_1*beta - v_2 + v_3 + k_f*Y - k*Z - VOCC;
+L_V =  F* V_cyto*(v_0 - k*Z + v_1*beta-VOCC);
 L_Y =  v_2 - v_3 - k_f*Y;
+
+%% From Goldbeter
+% L_Z =  v_0 + v_1*beta - v_2 + v_3 + k_f*Y - k*Z;
+% L_V =  F* V_cyto*(v_0 - k*Z + v_1*beta);
+% L_Y =  v_2 - v_3 - k_f*Y;
 
 end
 
